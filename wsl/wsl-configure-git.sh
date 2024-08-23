68a4fff9d98469622ce80437d57b8c7b8505ab0c
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2024-08-23 23:01:03 +0000

    gh auth
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/wsl-configure-git.sh
+++ wsl/wsl-configure-git.sh
@@ -5,7 +5,13 @@
 # The script will prompt you for two interactive authentication events with GitHub: the first sets the user
 # scope to allow reading user account data, and the second sets the default scope, which is more restrictive.
 
-gh auth login --hostname github.com --scopes user --web
+# Use Windows credential manager with git. See https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git
+set -e
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+git config --global credential.https://dev.azure.com.useHttpPath true
+
+echo "Logging into GitHub with user scope to retrieve user name and email address."
+gh auth login --git-protocol https --hostname github.com --scopes user --web
 
 gh_acct_name=$(gh api user --jq '.name')
 if [ -z "$gh_acct_name" ]; then
@@ -24,12 +30,9 @@ git config --global user.email "$gh_email"
 git config --global init.defaultBranch main
 
 # Downgrade to the default GH scope
-gh auth refresh --hostname github.com --web
-
-# Use Windows credential manager with git. See https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git
-git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
-git config --global credential.https://dev.azure.com.useHttpPath true
+gh auth refresh --hostname github.com --reset-scopes
 
 echo "Git has been configured with the following settings:"
 echo " - user.email: $(git config --global user.email)"
-echo " - user.name: $(git config --global user.email)"
+echo " - user.name: $(git config --global user.name)"
+Echo "Resetting the GitHub security scope to default."
