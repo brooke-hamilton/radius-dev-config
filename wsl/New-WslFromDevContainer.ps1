7d565133cd88a771bb923415fd18e39bde730f6e
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2024-11-25 23:43:05 -0500

    pester tests
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- wsl/New-WslFromDevContainer.ps1
+++ wsl/New-WslFromDevContainer.ps1
@@ -3,62 +3,106 @@
 Creates a WSL instance from a dev container specification.
 
 .DESCRIPTION
-
-
+ToDo: Describe the purpose of this script.
+ToDo: Describe the parameters of this script.
 #>
 
 param (
+    [Parameter(Mandatory = $false)]
     [string]$WorkspaceFolder = ".",
-    [string]$DevContainerJsonPath,
-    [string]$WslInstanceName
+    
+    [Parameter(Mandatory = $false)]
+    [string]$DevContainerJsonPath = $null,
+    
+    [Parameter(Mandatory = $false)]
+    [string]$WslInstanceName = $null,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrWhiteSpace()]
+    [string]$WslUserName = $Env:USERNAME
 )
+Set-StrictMode -Version 3.0
 $ErrorActionPreference = "Stop"
 
-# Find the dev container json file
-if (-not $DevContainerJsonPath) {
-    [string[]]$devContainerJson = Get-ChildItem -Path $WorkspaceFolder -Filter "devcontainer.json" -Recurse -File
-    if ($devContainerJson.Count -gt 1) {
-        throw "Multiple devcontainer.json files found. Please provide the DevContainerJsonPath parameter."
+function Find-DevContainerJsonFile {
+    # Find the dev container json file
+    if ($script:DevContainerJsonPath) {
+        if (-not (Test-Path -Path $DevContainerJsonPath -PathType Leaf)) {
+            throw "No devcontainer.json file found."
+        }
     }
-    $DevContainerJsonPath = $devContainerJson[0]
-} else {
-    if (-not (Test-Path -Path $DevContainerJsonPath -PathType Leaf)) {
-        throw "No devcontainer.json file found."
+    else {
+        [System.IO.FileInfo[]]$devContainerJson = Get-ChildItem -Path $WorkspaceFolder -Filter "devcontainer.json" -Recurse -File
+        if (-not $devContainerJson) {
+            throw "No devcontainer.json files found."
+        }
+        if ($devContainerJson.Length -gt 1) {
+            throw "Multiple devcontainer.json files found. Please provide the DevContainerJsonPath parameter."
+        }
+        $script:DevContainerJsonPath = $devContainerJson[0]
     }
 }
 
-# Read the devcontainer.json file
-$jsonContent = Get-Content -Path $DevContainerJsonPath -Raw | ConvertFrom-Json
+function Get-DevContainerName {
+    # Read the devcontainer.json file
+    $jsonContent = Get-Content -Path $DevContainerJsonPath -Raw | ConvertFrom-Json
 
-# Get the container name from the json content
-$containerName = $jsonContent.name
+    # Get the container name from the json content
+    $containerName = $jsonContent.name
 
-if (-not $containerName) {
-    throw "Could not find the name element in $DevContainerJsonPath."
+    if (-not $containerName) {
+        throw "Could not find the name element in $DevContainerJsonPath."
+    }
+
+    return $containerName.Replace(" ", "")
 }
 
-$containerName = $containerName.Replace(" ", "-") 
+function Invoke-ContainerBuild {
+    Write-Host "Building the container image $containerName for $DevContainerJsonPath..."
+
+    # Build the dev container
+    devcontainer build --workspace-folder="$WorkspaceFolder" --config="$DevContainerJsonPath" --image-name="$containerLabel" | Write-Host
+
+    # Run the dev container - the container will not run in wsl unless exported from a container instance instead of an image
+    Write-Host "Running the container image $containerLabel..."
+    docker run $containerLabel | Write-Host
+
+    $containerId = docker ps --latest --quiet
+    if (-not $containerId) {
+        throw "Could not find the container id."
+    }
+
+    Write-Host "Ran container $containerId"
+    return $containerId
+}
+
+function Set-UserAccount {
+    param (
+        [string]$wslInstanceName,
+        [string]$wslUserName
+    )
+    wsl --distribution $wslInstanceName -- usermod --login $wslUserName vscode
+    wsl --distribution $wslInstanceName -- usermod --home /home/$wslUserName -m $wslUserName
+    wsl --distribution $wslInstanceName -- groupmod --new-name $wslUserName vscode
+}
+
+Find-DevContainerJsonFile
+$containerName = Get-DevContainerName
 $containerLabel = $containerName.ToLower()
-#$tarFilePath = "c:\wsl\$containerName.tar"
+$containerId = Invoke-ContainerBuild
 
-Write-Host "Building the container image $containerName for $DevContainerJsonPath..."
-
-# Build the dev container
-devcontainer build --workspace-folder="$WorkspaceFolder" --config="$DevContainerJsonPath" --image-name="$containerLabel"
-
-# Run the dev container - the container will not run in wsl unless exported from a container instance instead of an image
-docker run $containerLabel
-
-$containerId = docker ps --latest --quiet
-if(-not $containerId) {
-    throw "Could not find the container id."
+# Set WslInstanceName
+if (-not $WslInstanceName) {
+    $WslInstanceName = $containerName
 }
+
+# ToDo: Calculate this path
+$wslInstancePath = "c:\wsl\$containerName"
 
 # Export the container to a tar file
-Write-Host "Importing to WSL instance $containerName..."
-docker export "$containerId" | wsl --import $containerName "c:\wsl\$containerName" -
+Write-Host "Importing WSL instance $WslInstanceName from container $containerId to $wslInstancePath ..."
+docker export "$containerId" | wsl --import $WslInstanceName $wslInstancePath -
 
-# Create the user account if it does not already exist
-wsl --distribution $containerName -- usermod --login brooke vscode
-wsl --distribution $containerName -- usermod --home /home/brooke -m brooke
-wsl --distribution $containerName -- groupmod --new-name brooke vscode
+Set-UserAccount -wslInstanceName $WslInstanceName -wslUserName $WslUserName
+
+# ToDo: Create /etc/wsl.conf file to set the default user
---
+++ wsl/New-WslFromDevContainer.tests.ps1
@@ -0,0 +1,157 @@
+Set-StrictMode -Version 3.0
+Describe 'New-WslFromDevContainer' {
+
+    BeforeAll {
+
+        function Get-DevContainerJsonContent {
+            #return $devContainerJson
+            return @'
+{
+    "name": "test-container",
+    "image": "mcr.microsoft.com/devcontainers/base:ubuntu"
+}
+'@
+        }
+
+        function New-DevContainerJsonFile {
+            param(
+                [string]$workspaceFolder,
+                [string]$subfolder = $null,
+                [string]$jsonContent
+            )
+    
+            #Create the .devcontainer folder if it does not exist
+            $devContainerFolder = Join-Path -Path $workspaceFolder -ChildPath '.devcontainer'
+            if(-not (Test-Path $devContainerFolder)) {
+                New-Item -ItemType Directory -Path $devContainerFolder | Out-Null
+            }
+    
+            #If the $subfolder parameter is provided and does not exist, create it
+            if($subfolder) {
+                $devContainerFolder = Join-Path -Path $devContainerFolder -ChildPath $subfolder
+                New-Item -ItemType Directory -Path $devContainerFolder | Out-Null
+            }
+            
+            $devContainerJsonPath = Join-Path -Path $devContainerFolder -ChildPath 'devcontainer.json'
+            $jsonContent | Set-Content -Path $devContainerJsonPath | Out-Null
+    
+            return $devContainerJsonPath
+        }
+
+        function Remove-WslInstance {
+            param(
+                [string]
+                [Parameter(Mandatory = $true)]
+                $wslInstanceName
+            )
+    
+            wsl --unregister $wslInstanceName
+            wsl --list | Should -Not -Contain $wslInstanceName
+        }
+
+        function Assert-WslInstance {
+            param(
+                [string]
+                [Parameter(Mandatory = $true)]
+                $wslInstanceName
+            )
+    
+            wsl --list | Should -Contain $wslInstanceName
+        }
+        
+        # Recreate the test data folder
+        $testDataPath = Join-Path -Path $PSScriptRoot -ChildPath 'TestData'
+        if ((Test-Path -Path $testDataPath)) {
+            Remove-Item -Path $testDataPath -Recurse -Force
+        }
+        New-Item -ItemType Directory -Path $testDataPath | Out-Null
+    }
+    BeforeEach {
+        $testDataPath | Should -Exist
+    }
+    AfterEach {
+        Get-ChildItem -Path $testDataPath -Recurse | Remove-Item -Recurse -Force
+    }
+    AfterAll {
+        Remove-Item -Path $testDataPath -Recurse -Force
+    }
+
+    It 'Throw error if no .devcontainer file' {
+        # Arrange
+        $expectedMessage = "No devcontainer.json files found."
+
+        # Act
+        { .\New-WslFromDevContainer -WorkspaceFolder $testDataPath } | Should -Throw $expectedMessage
+    }
+
+    It 'Throw error if multiple .devcontainer files and no json path parameter' {
+        # Arrange
+        $expectedMessage = "Multiple devcontainer.json files found. Please provide the DevContainerJsonPath parameter."
+        New-DevContainerJsonFile -workspaceFolder $testDataPath -subfolder 'subfolder1' -jsonContent (Get-DevContainerJsonContent) | Should -Exist
+        New-DevContainerJsonFile -workspaceFolder $testDataPath -subfolder 'subfolder2' -jsonContent (Get-DevContainerJsonContent) | Should -Exist
+        
+        # Act
+        { .\New-WslFromDevContainer -WorkspaceFolder $testDataPath } | Should -Throw $expectedMessage
+    }
+
+    It 'Throw error if invalid container path given' {
+        # Arrange
+        $expectedMessage = "No devcontainer.json file found."
+
+        # Act
+        { & .\New-WslFromDevContainer -WorkspaceFolder $testDataPath -DevContainerJsonPath 'invalid-path' } | Should -Throw $expectedMessage
+    }
+
+    It 'Throw error when null user name given' {
+        # Arrange
+        New-DevContainerJsonFile -workspaceFolder $testDataPath -jsonContent (Get-DevContainerJsonContent)
+        $expectedMessage = "Cannot validate argument on parameter 'WslUserName'. The argument is null, empty, or consists of only white-space characters. Provide an argument that contains non white-space characters, and then try the command again."
+
+        # Act
+        { & .\New-WslFromDevContainer -WorkspaceFolder $testDataPath -DevContainerJsonPath 'invalid-path' -WslUserName $null } | Should -Throw $expectedMessage
+    }
+
+    It 'Create WSL instance from workspace folder with one devcontainer.json' -Skip {
+        # Arrange
+        New-DevContainerJsonFile -workspaceFolder $testDataPath -jsonContent (Get-DevContainerJsonContent)
+        $wslInstanceName = 'test-container-wsl'
+        
+        # Act
+        { .\New-WslFromDevContainer -WorkspaceFolder $testDataPath -WslInstanceName $wslInstanceName } | Should -Not -Throw
+
+        # Assert
+        Assert-WslInstance -wslInstanceName $wslInstanceName
+        Remove-WslInstance -wslInstanceName $wslInstanceName
+    }
+
+    It 'Create WSL instance from workspace folder with multiple devcontainer.json files' -Skip {
+        # Arrange
+        New-DevContainerJsonFile -workspaceFolder $testDataPath -subfolder 'subfolder1' -jsonContent (Get-DevContainerJsonContent)
+        $devContainerJsonPath = New-DevContainerJsonFile -workspaceFolder $testDataPath -subfolder 'subfolder2' -jsonContent (Get-DevContainerJsonContent)
+        
+        $wslInstanceName = 'test-container-wsl'
+        
+        # Act
+        { .\New-WslFromDevContainer `
+            -WorkspaceFolder $testDataPath `
+            -DevContainerJsonPath $devContainerJsonPath `
+            -WslInstanceName $wslInstanceName } | Should -Not -Throw
+
+        # Assert
+        Assert-WslInstance -wslInstanceName $wslInstanceName
+        Remove-WslInstance -wslInstanceName $wslInstanceName
+    }
+
+    It 'Create WSL instance with default name' -Skip {
+        # Arrange
+        $devContainerJsonPath = New-DevContainerJsonFile -workspaceFolder $testDataPath -jsonContent (Get-DevContainerJsonContent)
+        $wslInstanceName = (Get-Content -Path $devContainerJsonPath -Raw | ConvertFrom-Json).name
+        
+        # Act
+        { .\New-WslFromDevContainer -WorkspaceFolder $testDataPath } | Should -Not -Throw
+
+        # Assert
+        Assert-WslInstance -wslInstanceName $wslInstanceName
+        Remove-WslInstance -wslInstanceName $wslInstanceName
+    }
+}
